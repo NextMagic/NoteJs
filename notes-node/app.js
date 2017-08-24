d1bd97411b8ca601f3d5fa36de3ca3a93476b5c2
N3xt <n3xt@N3xtde-MacBook-Pro.local>
2017-08-25 00:41:16 +0800

    [udemy] 3.23 Requiring Arguments and Advanced

--- notes-node/app.js
+++ notes-node/app.js
@@ -6,7 +6,31 @@ const notes = require('./notes.js')
 const _ = require('lodash'); 
 const yargs = require('yargs');
 
-const  argv = yargs.argv;
+const titleOptions = {
+    describe: 'Title of note',
+    demand: true,
+    alias :  't'
+};
+
+const bodyOpetions ={
+    describe: 'Body of note',
+    demand: true,
+    alias : 'b'    
+};
+const  argv = yargs
+              .command('add','Add a new note', {
+                title: titleOptions,
+                body: bodyOpetions
+              })
+              .command('list', 'List all notes')
+              .command('read' , 'Read a Notes',{
+                title: titleOptions
+              })
+              .command('remove','Remove a note', {
+                  title: titleOptions
+              })
+              .help()          
+              .argv;
 var command = argv._[0]
 
 console.log('Commnad: ',command);
