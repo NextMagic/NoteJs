4b07cc1df1211085f9d78265fe91d217e5d6979a
N3xt <n3xt@N3xtde-MacBook-Pro.local>
2017-08-25 16:00:34 +0800

    [Udemy] 4.30 WHat's Makes up an HTTP Request?

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,5 +4,8 @@ request({
     url:'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
     json: true
 }, (error ,response , body) => {
-    console.log(body);
+    console.log(JSON.stringify(`Address: ${body.results[0].formatted_address}`));
+    console.log(JSON.stringify(`Latitude: ${body.results[0].geometry.location.lat}`));
+    console.log(JSON.stringify(`Longititude: ${body.results[0].geometry.location.lng}`));
 });
+
