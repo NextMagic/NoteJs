4ed1e43b4996abd38a4696c879c059cdbf9a15de
N3xt <n3xt@N3xtde-MacBook-Pro.local>
2017-08-25 14:17:09 +0800

    [Udemy] 4.23 CallbackFunction & APIs

---
+++ weather-app/playground/async-basics.js
@@ -0,0 +1,12 @@
+console.log('Starting App');
+
+
+setTimeout(() => {
+    console.log('Inside of callback');
+}, 2000);
+
+setTimeout(() => {
+  console.log(' Second setTimeout');  
+} , 1000);
+
+console.log('Finishing App');
