95cad7e246c3e4dfd1e551cc203de84daddd9254
N3xt <n3xt@N3xtde-MacBook-Pro.local>
2017-08-27 11:14:16 +0800

    [Udemy] Encodeing User Inputer

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,22 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a:{
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .argv;
+
+var encodeAddress = encodeURIComponent(argv.address);
 
 request({
-    url:'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+    url:`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeAddress}`,
     json: true
 }, (error ,response , body) => {
     console.log(JSON.stringify(`Address: ${body.results[0].formatted_address}`));
